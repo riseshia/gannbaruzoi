f8a9deaae2704d4a1c957af1ba0a3d3e3cf6fa86
Shia <rise.shia@gmail.com>
2016-12-24 18:06:30 +0900

    add method for find dummy user

--- test/models/user_test.exs
+++ test/models/user_test.exs
@@ -6,6 +6,18 @@ defmodule Gannbaruzoi.UserTest do
   @valid_attrs %{email: "some content"}
   @invalid_attrs %{}
 
+  test "find_or_create_dummy with no registed user" do
+    User.find_or_create_dummy
+    assert 1 == Repo.aggregate(User, :count, :id)
+  end
+
+  test "find_or_create_dummy with registed user" do
+    expected_email = "exist@email.com"
+    %User{email: expected_email} |> Repo.insert!
+    user = User.find_or_create_dummy
+    assert expected_email == user.email
+  end
+
   test "changeset with valid attributes" do
     changeset = User.changeset(%User{}, @valid_attrs)
     assert changeset.valid?
