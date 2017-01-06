51317dfaea2f69e57045112a2390c13b38f0a80e
Shia <rise.shia@gmail.com>
2017-01-06 22:03:48 +0900

    Handle credo strict mode advice

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -23,14 +23,17 @@ defmodule Gannbaruzoi.UserSocket do
     {:ok, socket}
   end
 
-  # Socket id's are topics that allow you to identify all sockets for a given user:
+  # Socket id's are topics that allow you to identify all sockets
+  # for a given user:
   #
   #     def id(socket), do: "users_socket:#{socket.assigns.user_id}"
   #
   # Would allow you to broadcast a "disconnect" event and terminate
   # all active sockets and channels for a given user:
   #
-  #     Gannbaruzoi.Endpoint.broadcast("users_socket:#{user.id}", "disconnect", %{})
+  #     Gannbaruzoi.Endpoint.broadcast(
+  #       "users_socket:#{user.id}", "disconnect", %{}
+  #     )
   #
   # Returning `nil` makes this socket anonymous.
   def id(_socket), do: nil
