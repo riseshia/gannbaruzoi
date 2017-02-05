e03f7211e41428d590fe684bdeaf39a9a32f2fac
Shia <rise.shia@gmail.com>
2017-02-05 15:34:05 +0900

    Add Task enum (#37)

--- lib/types.ex
+++ lib/types.ex
@@ -10,13 +10,19 @@ defmodule Gannbaruzoi.Types do
     field :id, non_null(:id)
     field :description, non_null(:string)
     field :estimated_size, non_null(:integer)
-    field :type, non_null(:string)
+    field :type, non_null(:task_enum)
     field :parent_id, :id
     field :status, non_null(:boolean)
   end
 
   @desc "Connection of Tasks"
   connection node_type: :task
+
+  @desc "Task type"
+  enum :task_enum do
+    value :branch, as: "branch", description: "Sub-task"
+    value :root, as: "root", description: "Root-task"
+  end
 
   @desc "A log"
   object :log do
