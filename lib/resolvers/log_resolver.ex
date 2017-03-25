f4a6f2705afad5cc577caa5c171566a3c07a9359
Shia <rise.shia@gmail.com>
2017-03-25 15:44:05 +0900

    Support task.logs scheme (#45)
    * Add logs to Tasks query
    
    * Resolve N+1 problem

--- lib/resolvers/log_resolver.ex
+++ lib/resolvers/log_resolver.ex
@@ -1,8 +1,16 @@
 defmodule Gannbaruzoi.LogResolver do
+  import Absinthe.Resolution.Helpers
+
   @moduledoc """
   The resolvers of log
   """
   alias Gannbaruzoi.{Repo, Log}
+
+  def all(task, _, _) do
+    batch({__MODULE__, :by_task_ids}, task.id, fn batch_results ->
+      {:ok, Map.get(batch_results, task.id, [])}
+    end)
+  end
 
   def create(%{task_id: task_id}, _info) do
     log =
@@ -16,5 +24,12 @@ defmodule Gannbaruzoi.LogResolver do
     log = Log |> Log.by_task_id(task_id) |> Log.recent() |> Repo.one()
     Repo.delete(log)
     {:ok, %{id: log.id}}
+  end
+
+  def by_task_ids(_, task_ids) do
+    Log
+    |> Log.by_task_ids(task_ids)
+    |> Repo.all
+    |> Enum.group_by(&(&1.task_id))
   end
 end
